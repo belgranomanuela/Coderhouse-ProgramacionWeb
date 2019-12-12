21edca3c8613cf218726b0db02657173cd38773f
Manuela <mbelgranoo@hotmail.com>
2019-12-12 15:58:53 -0300

    Workshop final

--- Clase 17 - 18/Alumno/Workshop final/Desafío/src/js/controllers/guardadosController.js
+++ Clase 17 - 18/Alumno/Workshop final/Desafío/src/js/controllers/guardadosController.js
@@ -1,5 +1,39 @@
+import {
+  getLocalList,
+  setLocalList
+} from '../utils/localStorage'
+
+
 function guardadosController() {
-  console.log('Se cargaron los personajes guardados');
+  let datos = getLocalList('Personajes')
+  for (let i = 0; i < datos.length; i++) {
+    const personaje = datos[i];
+    $('#tableBody').append("<tr><td> " + i + "</td><td>" + personaje.nombre + "</td><td>" + personaje.genero + "</td><td>" + personaje.altura + "</td><td>" + personaje.peso + "</td><td>" + personaje.ojos + "</td><td><button class = 'btn btn-danger'>Elminar</button></td></tr>")
+  }
+
+  var buttonNode = $('.btn')
+  buttonNode.click(function () {
+    let fila = $(this).parent().parent();
+    let nombre = fila.children()[1].innerHTML;
+
+    var posicionAEliminar;
+
+    for (let i = 0; i < datos.length; i++) {
+      var eliminarPersonaje = datos[i];
+      if (nombre === eliminarPersonaje.nombre) {
+        posicionAEliminar = i;
+      }
+    }
+
+    datos.splice(posicionAEliminar, 1)
+
+    setLocalList('Personajes', datos)
+
+    $(this).parent().parent().fadeOut(500, function () {
+      console.log('Se completo la animación')
+    })
+  })
 }
+
 
 export default guardadosController;
