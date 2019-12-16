81d12591b4a022d582c7d28b90ab4a48b429b5be
Manuela <mbelgranoo@hotmail.com>
2019-12-15 21:42:10 -0300

    Terminé Proyecto Final

--- Clase 17 - 18/Alumno/Workshop final/Desafío/src/js/controllers/guardadosController.js
+++ Clase 17 - 18/Alumno/Workshop final/Desafío/src/js/controllers/guardadosController.js
@@ -1,39 +1,53 @@
 import {
   getLocalList,
   setLocalList
-} from '../utils/localStorage'
+} from "../utils/localStorage";
 
+let personajes = [];
 
 function guardadosController() {
-  let datos = getLocalList('Personajes')
-  for (let i = 0; i < datos.length; i++) {
-    const personaje = datos[i];
-    $('#tableBody').append("<tr><td> " + i + "</td><td>" + personaje.nombre + "</td><td>" + personaje.genero + "</td><td>" + personaje.altura + "</td><td>" + personaje.peso + "</td><td>" + personaje.ojos + "</td><td><button class = 'btn btn-danger'>Elminar</button></td></tr>")
+  personajes = getLocalList("Personajes");
+
+  for (let i = 0; i < personajes.length; i++) {
+    const personaje = personajes[i];
+    $("#tableBody").append(
+      "<tr><td> " +
+      i +
+      "</td><td>" +
+      personaje.nombre +
+      "</td><td>" +
+      personaje.genero +
+      "</td><td>" +
+      personaje.altura +
+      "</td><td>" +
+      personaje.peso +
+      "</td><td>" +
+      personaje.ojos +
+      "</td><td><button class = 'btn btn-danger'>Elminar</button></td></tr>"
+    );
   }
-
-  var buttonNode = $('.btn')
-  buttonNode.click(function () {
-    let fila = $(this).parent().parent();
-    let nombre = fila.children()[1].innerHTML;
-
-    var posicionAEliminar;
-
-    for (let i = 0; i < datos.length; i++) {
-      var eliminarPersonaje = datos[i];
-      if (nombre === eliminarPersonaje.nombre) {
-        posicionAEliminar = i;
-      }
-    }
-
-    datos.splice(posicionAEliminar, 1)
-
-    setLocalList('Personajes', datos)
-
-    $(this).parent().parent().fadeOut(500, function () {
-      console.log('Se completo la animación')
-    })
-  })
 }
 
+$("body").on("click", ".btn-danger", function () {
+  let fila = $(this)
+    .parent()
+    .parent();
+
+  let nombre = fila.children()[1].innerHTML;
+
+  var posicionAEliminar;
+
+  for (let i = 0; i < personajes.length; i++) {
+    var eliminarPersonaje = personajes[i];
+    if (nombre === eliminarPersonaje.nombre) {
+      posicionAEliminar = i;
+    }
+  }
+
+  personajes.splice(posicionAEliminar, 1);
+  setLocalList("Personajes", personajes);
+
+  fila.hide();
+});
 
 export default guardadosController;
